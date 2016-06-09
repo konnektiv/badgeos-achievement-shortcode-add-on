6ebf750429407a5059759adabb26f98bce7018fa
Christoph Herbst <chris.p.herbst@gmail.com>
2016-06-09 10:08:28 +0200

    activate select2 for achievement id attribute

--- js/achievement-shortcode-embed.js
+++ js/achievement-shortcode-embed.js
@@ -52,4 +52,35 @@
 		tb_remove();
 	});
 
+	$( '#user_earned_achievement_id' ).select2( {
+		ajax: {
+			url: ajaxurl,
+			type: 'POST',
+			data: function( term ) {
+				return {
+					q: term,
+					action: 'get-achievements-select2',
+				};
+			},
+			results: function( results, page ) {
+				console.log(results);
+				return {
+					results: results.data
+				};
+			}
+		},
+		id: function( item ) {
+			return item.ID;
+		},
+		formatResult: function ( item ) {
+			return item.post_title;
+		},
+		formatSelection: function ( item ) {
+			return item.post_title;
+		},
+		placeholder: badgeos_shortcode_embed_messages.id_placeholder,
+		allowClear: true,
+		multiple: false
+	} );
+
 }(jQuery));
